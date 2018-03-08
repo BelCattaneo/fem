363c512a13b535ae8ad38747c151e1164024834e
belcattaneo <belcattaneo@gmail.com>
2018-03-08 17:20:32 -0300

    Diferencias Finitas completed.

--- helpers.js
+++ helpers.js
@@ -1,3 +1,4 @@
+const math = require("mathjs");
 function matrixIndexes(matrix, fn) {
   matrix.forEach((row, y) => {
     row.forEach((node, x) => {
@@ -91,7 +92,78 @@ function createMatrix(temperatures, size) {
 }
 
 
-var matrix = createMatrix({top: 10, right: 20, bottom: 30, left: 40}, 4);
+let matrix = createMatrix({top: 10, right: 20, bottom: 30, left: 40}, 4);
 console.log(matrix)
 
-console.log(buildConstantMatrix(matrix));
+let constantMatrix = buildConstantMatrix(matrix);
+console.log(constantMatrix);
+
+function buildCoeficientMatrix(size){
+  const matrix = [];
+  for(let j = 0; j < size; j++) {
+    matrix.push([]);
+    for(let i = 0; i < size; i++) {
+      if(i===j){
+        matrix[j][i] = 1;
+      } else if(i+j === size-1){
+        matrix[j][i] = 0;
+      } else {
+        matrix[j][i] = -1/4;
+      }
+    }
+  }
+  return matrix; 
+}
+let coeficientMatrix = buildCoeficientMatrix(4);
+console.log(coeficientMatrix);
+
+let determinant = math.det(coeficientMatrix);
+console.log(determinant);
+
+function clone(obj){
+  return JSON.parse(JSON.stringify(obj));
+}
+
+function replaceWithConstantMatrix(coeficientMatrix, constantMatrix, index){
+  let newCoeficientMatrix = clone(coeficientMatrix);
+  for (let i = 0; i < constantMatrix.length; i++) {
+    newCoeficientMatrix[i][index] = constantMatrix[i];    
+  }
+
+  return newCoeficientMatrix;
+}
+console.log(replaceWithConstantMatrix([[1,1,1],[1,1,1],[1,1,1]], [55,22,55], 0));
+
+function getResults(coeficientMatrix, constantMatrix){
+  const determinant = math.det(coeficientMatrix);
+  console.log(determinant);
+  let results = [];
+
+  console.log(coeficientMatrix);
+  for(i = 0; i < constantMatrix.length; i++){
+    console.log(constantMatrix);
+    console.log(math.det(replaceWithConstantMatrix(coeficientMatrix, constantMatrix, i)));
+    results.push((math.det(replaceWithConstantMatrix(coeficientMatrix, constantMatrix, i)) / determinant)/4);
+  }
+
+  return results;
+}
+
+let results = getResults(coeficientMatrix, constantMatrix);
+console.log(results);
+
+function buildFinalMatrix(matrix, results){
+  let index = 0;
+  let finalMatrix = clone(matrix);
+  for (let i = 0; i < finalMatrix.length; i++) {
+    for (let j = 0; j < finalMatrix[i].length; j++) {
+      if (finalMatrix[i][j] === 0) {
+        finalMatrix[i][j] = results[index];
+        index++;
+      }      
+    }
+  }
+  return finalMatrix;
+}
+
+console.log(buildFinalMatrix(matrix, results));
