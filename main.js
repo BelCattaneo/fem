2d67262757a7de341b4e82e1207173c9f4fea2e9
belcattaneo <belcattaneo@gmail.com>
2018-03-08 14:59:11 -0300

    Using mathjs.

--- main.js
+++ main.js
@@ -1,31 +1,10 @@
 const screenSize = 600;
 
-
-function totalNodes(totalElements) {
-  if(totalElements === 1) {
-    return 4
-  } else {
-    return 2 + totalNodes(totalElements - 1);
-  }
-}
-
-
 $(document).ready(function() {
-
+  /*
   const heatmap = h337.create({
     container: $(".heatmap")[0]
   });
-
-
-  heatmap.setData({
-    max: 100,
-    data: [
-      { x: 0, y: 0 , value: 100},
-      { x: 20, y: 0 , value: 100},
-      { x: 1859, y: 0 , value: 100},
-      { x: 1859, y: 0 , value: 100},
-      { x: 1859, y: 0 , value: 100}
-    ]
-  });
+  */
 
 });
